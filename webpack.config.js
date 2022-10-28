0c3cf58a816cef713b3995baa5417ef0d0ab1d4a
Tsuna <beatles1964@yandex.ru>
2022-10-28 12:02:20 +0300

    Fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const Dotenv = require('dotenv-webpack')
 module.exports = {
   entry: path.join(__dirname, 'src', 'index.js'),
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'build'),
     filename: 'webpack.bundle.js',
     publicPath: '/',
   },
